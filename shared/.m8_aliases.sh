7391577c2d3b2c2b6882e7154a8ae6fc3f92114f
Sebastian Caicedo <jsebdev@gmail.com>
2025-08-27 15:28:17 -0500

    update django task run command

--- shared/.m8_aliases.sh
+++ shared/.m8_aliases.sh
@@ -1,22 +1,27 @@
 alias cdsw='cd /mnt/c/Users/sebas/coding/macheight/safefreight'
 alias cdrc='cd ~/coding/macheight/onerallypoint/rallyclaim'
 
-alias lrcst="aws ecs list-tasks --cluster rally-claim-cluster-staging"
-alias lrcpt="aws ecs list-tasks --cluster rally-claim-cluster-prod"
+alias lrctt="aws ecs list-tasks --cluster rally-claim-cluster-test"
+alias lrcst="aws ecs list-tasks --cluster rally-claim-cluster-stage"
 
 rallyclaim_run_django_task_terminal() {
-    local environment="${1:-staging}"
-    if [[ "$environment" != "staging" && "$environment" != "prod" ]]; then
-        echo "Invalid environment. Use 'staging' or 'prod'."
+    local environment="${1:-test}"
+    if [[ "$environment" != "test" && "$environment" != "stage" ]]; then
+        echo "Invalid environment. Use 'test' or 'stage'."
         return 1
     fi
     echo "Opening Django task in $environment environment..."
-    local task_id=$(aws ecs list-tasks --cluster rally-claim-cluster-staging --query 'taskArns[0]' --output text)
+    local cluster_name="rally-claim-$environment-cluster"
+    echo "Cluster Name: $cluster_name"
+    # local task_id=$(aws ecs list-tasks --cluster $cluster_name --family django-app --query 'taskArns[0]' --output text)
+    local task_id=$(aws ecs list-tasks --cluster $cluster_name --query 'taskArns[0]' --output text)
     echo "Task ID: $task_id"
+    local container_name="rally-claim-$environment-app"
+    echo "Container Name: $container_name"
     aws ecs execute-command \
-      --cluster rally-claim-cluster-$environment \
+      --cluster $cluster_name \
       --task $task_id \
-      --container django-app \
+      --container $container_name \
       --interactive \
       --command "/bin/bash"
 }
