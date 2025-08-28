13d274cc6f7b2107c176a2becfce1485889fc36a
Sebastian Caicedo <jsebdev@gmail.com>
2025-08-28 09:29:48 -0500

    fix login to rallyclaim task

--- shared/.m8_aliases.sh
+++ shared/.m8_aliases.sh
@@ -1,9 +1,6 @@
 alias cdsw='cd /mnt/c/Users/sebas/coding/macheight/safefreight'
 alias cdrc='cd ~/coding/macheight/onerallypoint/rallyclaim'
 
-alias lrctt="aws ecs list-tasks --cluster rally-claim-cluster-test"
-alias lrcst="aws ecs list-tasks --cluster rally-claim-cluster-stage"
-
 rallyclaim_run_django_task_terminal() {
     local environment="${1:-test}"
     if [[ "$environment" != "test" && "$environment" != "stage" ]]; then
@@ -13,11 +10,16 @@ rallyclaim_run_django_task_terminal() {
     echo "Opening Django task in $environment environment..."
     local cluster_name="rally-claim-$environment-cluster"
     echo "Cluster Name: $cluster_name"
-    # local task_id=$(aws ecs list-tasks --cluster $cluster_name --family django-app --query 'taskArns[0]' --output text)
-    local task_id=$(aws ecs list-tasks --cluster $cluster_name --query 'taskArns[0]' --output text)
+
+    local task_id=$(aws ecs list-tasks \
+        --cluster "$cluster_name" \
+        --query 'taskArns[0]' \
+        --output text)
     echo "Task ID: $task_id"
+
     local container_name="rally-claim-$environment-app"
     echo "Container Name: $container_name"
+
     aws ecs execute-command \
       --cluster $cluster_name \
       --task $task_id \
@@ -32,6 +34,6 @@ rallyclaim_lints() {
     docker compose run --rm react npm run lint
     docker compose run --rm react npm run typecheck
     docker compose run --rm django mypy .
-    docker compose run --rm django ruff check .
+    docker compose run --rm django ruff check . --fix
 }
 
