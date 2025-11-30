da524420e017eb0fc05cda3209e4aa8844b42b17
Sebastian Caicedo <jsebdev@gmail.com>
2025-11-30 14:53:26 -0500

    function to convert from .ipynb to .ju.pyb

--- shared/.shared_aliases.sh
+++ shared/.shared_aliases.sh
@@ -9,3 +9,15 @@ alias cddf="cd ~/coding/dotfiles/"
 alias cdsf="cd ~/coding/macheight/safefreight/repo/"
 
 load_if_exists ~/.m8_aliases.sh
+
+
+# jupyter notebooks
+ipynb2jupy() {
+  local file_to_convert="$1"
+  if [[ -z "$file_to_convert" ]]; then
+    echo "Usage: ipynb2jupy <notebook_file.ipynb>"
+    return 1
+  fi
+  local filename_without_extension="${file_to_convert%.ipynb}"
+  jupytext --to py:percent "$file_to_convert" -o "${filename_without_extension}.ju.py"
+}
