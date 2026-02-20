c4a3fc2343c3a89c21cb8df5436f12b4da37bd48
Sebastian Caicedo <jsebdev@gmail.com>
2026-02-20 14:19:27 -0500

    use real repos

--- shared/.shared_environment.sh
+++ shared/.shared_environment.sh
@@ -1,6 +1,6 @@
 # for arena.io
 export GITHUB_PACKAGES_USER=sebastianMacheight
-export GO_ARENA_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/dummy_repo"
-export PERSON_SERVICE_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/dummy_repo"
+export GO_ARENA_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/go-arena"
+export PERSON_SERVICE_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/person-service"
 export QUESTIONNAIRE_V2_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/questionnaire-v2"
 export CLIENT_CONFIG_GIT_ROOT="$HOME/coding/macheight/arena.io/repos/client-config"
