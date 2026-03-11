5e5dddbbdc0c6e4bb0020e3665f9b4d93a7ff3aa
Sebastian Caicedo <jsebdev@gmail.com>
2026-03-11 11:04:09 -0500

    connect to arena questionnaire local db

--- shared/.m8_functions.sh
+++ shared/.m8_functions.sh
@@ -138,3 +138,7 @@ def execute_command_in_arena_client_config() {
     fi
     docker exec -u vscode  $(docker ps | grep client-config | awk '{print $NF}') bash -c "export PATH=\"\$HOME/.local/share/mise/shims:\$PATH\" && cd /workspaces/client-config && $command"
 }
+
+def connect_to_local_arena_questionnaire_v2_db() {
+    mongosh "mongodb://user:pass@localhost:27050/questionnaire?authSource=admin"
+}
