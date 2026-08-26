46877acce16b41e6ab19e5e217d0f56e4799145f
Sebastian Caicedo <jsebdev@gmail.com>
2026-08-26 02:58:19 -0500

    remove mfa env var

--- shared/.m8_environment.sh
+++ shared/.m8_environment.sh
@@ -1,1 +1,2 @@
-export RALLYCLAIM_MFA_SERIAL="arn:aws:iam::102282313732:mfa/keepass_TOTP"
+# Removed this env variable because I want to be able to fetch it using my aws_login command
+# export RALLYCLAIM_MFA_SERIAL="arn:aws:iam::102282313732:mfa/keepass_TOTP"
